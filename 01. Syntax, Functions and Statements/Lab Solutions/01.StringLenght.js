a3db4bf31c00d573a7246295a3806d9b82e42735
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-11 17:56:52 +0300

    01.StringLenght solution added

---
+++ 01. Syntax, Functions and Statements/Lab Solutions/01.StringLenght.js
@@ -0,0 +1,8 @@
+function solve(x, y, z){
+    let sum = x.length + y.length + z.length;
+    let avrg = Math.floor(sum/3);
+    console.log(sum);
+    console.log(avrg);
+}
+
+solve('chocolate', 'ice-cream', 'cake');
