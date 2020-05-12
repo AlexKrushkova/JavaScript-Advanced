56be40e024e58bb27d5e60cd29ae20f90b120464
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-12 09:36:23 +0300

    Lab solution to 05.CircleArea is added

---
+++ 01. Syntax, Functions and Statements/Lab Solutions/05.CircleArea.js
@@ -0,0 +1,15 @@
+function solve (input){
+    let result;
+    let inputType = typeof input;
+
+    if (inputType === "number"){
+        result = (Math.pow(input, 2) * Math.PI).toFixed(2);
+    }
+    else {
+        result = `We can not calculate the circle area, because we receive a ${inputType}.`
+    }
+    return result;
+}
+
+// console.log(solve(5));
+// console.log(solve('name'));
