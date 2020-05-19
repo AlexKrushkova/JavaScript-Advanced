ee0e8aa8aafab63a8ecee37ab8ea8f88d9e12cd4
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-19 16:37:12 +0300

    Exercise solutions updated

--- 05. DOM Manipulation/Exercise solutions/01. Sections/script.js
+++ 05. DOM Manipulation/Exercise solutions/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let container = document.querySelector('#content');
+
+   for (let word of words) {
+       let divElement = document.createElement('div');
+       let pElement = document.createElement('p');
+
+       pElement.textContent = word;
+       pElement.style.display = 'none';
+
+       divElement.appendChild(pElement);
+       divElement.addEventListener('click', () => {
+           pElement.style.display = 'inline-block';
+       });
+
+       container.appendChild(divElement);
+   }
 }
