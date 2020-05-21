76876685096e428315dca4e4642ba57f2d24cbc5
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-21 15:46:22 +0300

    solutions to lab updated

---
+++ 07. Function Context/Lab Solutions/05. Spy.js
@@ -0,0 +1,12 @@
+  
+function Spy(obj, method) {
+    let originalFunction = obj[method];
+    let invoked = {
+        count: 0
+    }
+    obj[method] = function(){
+        invoked.count++;
+        return originalFunction.apply(this, arguments)
+    }
+    return invoked;
+}
