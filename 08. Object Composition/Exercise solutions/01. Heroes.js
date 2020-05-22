8bc062aa57c879d2a01829bd4b69e6081cec4413
AlexKrushkova <alexkrushkova@gmail.com>
2020-05-22 13:09:18 +0300

    Solutions to Exercises updated

--- 08. Object Composition/Exercise solutions/01. Heroes.js
+++ 08. Object Composition/Exercise solutions/01. Heroes.js
@@ -32,15 +32,3 @@ function solve() {
   };
   return { mage: mage, fighter: fighter };
 }
-
-// let create = solve();
-// const scorcher = create.mage("Scorcher");
-// scorcher.cast("fireball")
-// scorcher.cast("thunder")
-// scorcher.cast("light")
-
-// const scorcher2 = create.fighter("Scorcher 2");
-// scorcher2.fight()
-
-// console.log(scorcher2.stamina);
-// console.log(scorcher.mana);
