e2bfb5de44ac80097786dc9cf9881e448571312c
alexkrushkova <alexkrushkova@gmail.com>
2020-06-22 10:44:55 +0300

    Solution to 01.Christmas gift delivery updated

--- 10. Exam Prep/10 December 2019/Solutions/01.Christmas-Gift-Delivery/app.js
+++ 10. Exam Prep/10 December 2019/Solutions/01.Christmas-Gift-Delivery/app.js
@@ -1,3 +1,58 @@
 function solution() {
-    //TO DO
+    let addBtn = document.querySelector(".card > div > button");
+    let inputGiftName = document.querySelector(".card > div > input");
+
+    let listOfGifts = document.querySelector(".card > ul");
+    let giftList = [];
+
+    let sentGifts = document.querySelector("body > div > section:nth-child(3) > ul");
+    let discaredGifts = document.querySelector("body > div > section:nth-child(4) > ul");
+
+    addBtn.addEventListener("click", createGift);
+
+    function createGift(e) { // e.preventDefault();
+        listOfGifts.innerHTML = "";
+        let giftName = inputGiftName.value;
+        giftList.push(giftName);
+
+        giftList.sort((a, b) => a.localeCompare(b));
+        for (let i = 0; i < giftList.length; i++) {
+            let currGift = giftList[i];
+
+            let newItemList = document.createElement("li");
+            newItemList.classList.add("gift");
+            newItemList.textContent = currGift;
+
+            let sendButton = document.createElement("button");
+            sendButton.id = "sendButton";
+            sendButton.innerText = "Send";
+
+            let discardButton = document.createElement("button");
+            discardButton.id = "discardButton";
+            discardButton.innerText = "Discard";
+
+            newItemList.appendChild(sendButton);
+            newItemList.appendChild(discardButton);
+
+            listOfGifts.appendChild(newItemList);
+            inputGiftName.value = "";
+
+
+            sendButton.addEventListener("click", function () {
+                listOfGifts.removeChild(newItemList);
+                newItemList.removeChild(sendButton);
+                newItemList.removeChild(discardButton);
+                sentGifts.appendChild(newItemList);
+                giftList = [];
+            });
+
+            discardButton.addEventListener("click", function(){
+                listOfGifts.removeChild(newItemList);
+                newItemList.removeChild(sendButton);
+                newItemList.removeChild(discardButton);
+                discaredGifts.appendChild(newItemList);
+                giftList = [];
+            })
+        }
+    }
 }
